c1949f360d516bfa352e3400f0523bc40587657c
Jesús Moreno León <jesus.moreno@programamos.es>
2016-08-09 10:30:55 -0400

    Add Variables class

--- hairball/plugins/neu.py
+++ hairball/plugins/neu.py
@@ -1,1 +1,23 @@
+"""This module provides plugins for NEU metrics."""
 
+import kurt
+from hairball.plugins import HairballPlugin
+
+
+class Variables(HairballPlugin):
+
+    """Plugin that counts the number of variables in a project."""
+
+    def __init__(self):
+        super(Variables, self).__init__()
+        self.total = 0
+
+    def finalize(self):
+        """Output the number of variables in the project."""
+        print("Number of variables: %i" % self.total)
+
+    def analyze(self, scratch):
+        """Run and return the results of the Variables plugin."""          
+        self.total = len(scratch.variables)
+        for x in scratch.sprites:
+            self.total += len(x.variables)
