7b3235ee587727fd1d3407162b6c0002a9a3aad1
Jesús Moreno León <jesus.moreno@programamos.es>
2016-10-03 12:03:55 -0400

    Make duplicate.py output more friendly

--- hairball/plugins/duplicate.py
+++ hairball/plugins/duplicate.py
@@ -11,12 +11,13 @@ class DuplicateScripts(HairballPlugin):
         super(DuplicateScripts, self).__init__()
         self.total_duplicate = 0
         self.list_duplicate = []
+        self.list_duplicate_string = []
 
     def finalize(self):
         """Output the duplicate scripts detected."""
         if self.total_duplicate > 0:
-            print("%d duplicate scripts found" % self.total_duplicate)
-            for duplicate in self.list_duplicate:
+            print "%d duplicate scripts found" % self.total_duplicate
+            for duplicate in self.list_duplicate_string:
                 print duplicate
 
     def analyze(self, scratch):
@@ -27,14 +28,17 @@ class DuplicateScripts(HairballPlugin):
             #Scripts defined by user are not considered
             if script[0].type.text != 'define %s':  
                 blocks_list = []
-                for name, _, _ in self.iter_blocks(script.blocks):
+                blocks_list_string = []
+                for name, _, block in self.iter_blocks(script.blocks):
                     blocks_list.append(name)
+                    blocks_list_string.append(block.stringify())    
                 blocks_tuple = tuple(blocks_list)
                 if blocks_tuple in scripts_set:
-                    if len(blocks_list)>5:
-			if not blocks_list in self.list_duplicate:
+                    if len(blocks_list) > 5:
+                        if not blocks_list in self.list_duplicate:
                             self.total_duplicate += 1
                             self.list_duplicate.append(blocks_list)
+                            self.list_duplicate_string.append(blocks_list_string)
                 else:
                     scripts_set.add(blocks_tuple)
 
