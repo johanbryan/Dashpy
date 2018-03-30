8bd4ac9e3f1942b00b98839f072a0dbc42b1bd1a
Joaquin Camacho <ssrmatu@gmail.com>
2018-03-30 09:50:46 -0400

    Modified drawing.py to contain more plugins

--- hairball/plugins/drawing.py
+++ hairball/plugins/drawing.py
@@ -1,26 +1,57 @@
-"""This module provides plugins for Fascinating Animate Drawing criterias 
-from PRG (Progracademy) inspired on code.org challenges."""
+from collections import Counter
 
 import kurt
-
-from collections import Counter
 from hairball.plugins import HairballPlugin
 
-class Color(HairballPlugin): 
-    """Plugin that checks if a Scrath Project uses at least two colors."""
+class Drawing(HairballPlugin): 
+    """This class provides plugins for Fascinating Animate Drawing criterias 
+    from PRG (Progracademy) inspired on code.org challenges."""
+
+    MAX_USEOFCOLOR_SCORE = 2
+    MAX_MOVEOFARTIST_SCORE = 6
+    MAX_NESTEDLOOP_SCORE = 6
+    MAX_GEOMETRICFIGURE_SCORE = 6
+
+    MAX_SCORE = MAX_USEOFCOLOR_SCORE + MAX_MOVEOFARTIST_SCORE + MAX_NESTEDLOOP_SCORE + MAX_GEOMETRICFIGURE_SCORE
+
+    RANGE_NAME = ['Basic', 'Developing', 'Proficiency']
+
+    DIFFICULTY = 0.5 #0 = Easy, 0.5 = Normal, 1 = Hard
+    LEVEL_RANGE = float(MAX_SCORE)/len(RANGE_NAME)
+
 
     COLOR_BLOCK = ['set pen color to %s', 
                    'change pen color by %s',
                    'set pen color to %s']
 
     def __init__(self):
-        super(Color, self).__init__()
-        self.__block_result = Counter()
-        self.__argument_result = Counter()
-        self.__final_result = {'color' : 0}
+        """Construct method"""
+        super(Drawing, self).__init__()
+        self.__final_result = {}
 
     def analyze(self, scratch):
-        """Run and return the results from the Color plugin."""
+        """Run and return the results from the Drawing plugins."""
+        self.__final_result['Use Of Color'] = self.__analyzeUseOfColor(scratch)
+        #self.__final_result['Move Of Artist'] = self.__analyzeMoveOfArtist(scratch)
+        #self.__final_result['Nested Loop'] = self.__analyzeNestedLoop(scratch)
+        #self.__final_result['Geometric Figure'] = self.__analyzeGeomtricFigure(scratch)
+
+    def finalize(self):
+        """Print in command prompt the final results"""
+        #Variables
+        score = reduce(lambda a,b: a+b, self.__final_result.values())
+        #Evalutes the range score of Scratch project
+        self.__final_result['Score'] = score
+        self.__final_result['Max Score'] = self.MAX_SCORE
+        self.__final_result['Range'] = self.RANGE_NAME[int(score / self.LEVEL_RANGE + self.DIFFICULTY - 1)]
+        print self.__final_result
+    
+    #_____________PRIVATE METHODS_____________#
+    def __analyzeUseOfColor(self, scratch):
+        """Plugin that checks if a Scrath Project uses at least two colors."""
+        #Variables
+        block_result = Counter()
+        argument_result = Counter()
         #Extrae los scripts del proyecto Scratch
         for script in self.iter_scripts(scratch):
             if self.script_start_type(script) != self.NO_HAT:
@@ -30,19 +61,16 @@ class Color(HairballPlugin):
                     #Si coincide almacena el bloque en block_results y los parametros del bloque en argument_results
                     for color in self.COLOR_BLOCK:
                         if color in name:
-                            self.__block_result[name] += 1
+                            block_result[name] += 1
                             if isinstance(block.args[0], kurt.Color):
-                                self.__argument_result[block.args[0].stringify()] += 1
+                                argument_result[block.args[0].stringify()] += 1
                             else:
-                                self.__argument_result[True] += 1
+                                argument_result[True] += 1
         #Evalua si el proyecto cumple el criterio Color
-        if self.__block_result:
-            if len(self.__argument_result.items()) == 1:
-                self.__final_result['color'] = 1
-            if len(self.__argument_result.items()) >= 2:
-                self.__final_result['color'] = 2
-        
-    def finalize(self):
-        print self.__final_result, self.__block_result, self.__argument_result
-        
-        
+        if block_result:
+            if len(argument_result) == 1:
+                return self.MAX_USEOFCOLOR_SCORE / 2
+            else:
+                return self.MAX_USEOFCOLOR_SCORE
+        else:
+            return 0
